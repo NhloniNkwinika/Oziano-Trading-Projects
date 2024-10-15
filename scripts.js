5f1f19556ae7e9b9735282ac4ec7ef1261d2629c
NhloniNkwinika <hloninkwinika@gmail.com>
2024-10-15 16:36:40 +0200

    .

--- scripts.js
+++ scripts.js
@@ -16,3 +16,60 @@ navLinks.forEach(link => {
         burger.classList.remove('toggle');
     });
 });
+
+///
+
+
+ 
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const content = document.querySelector('.content-to-animate');
+
+    
+    function isElementInViewport(el) {
+      const rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+      );
+    }
+
+    
+    function handleScroll() {
+      if (isElementInViewport(content)) {
+        content.classList.add('content-visible');
+        window.removeEventListener('scroll', handleScroll);
+      }
+    }
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+  });
+
+
+
+
+
+  
+  
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+ 
+
+ 
+
+ 
